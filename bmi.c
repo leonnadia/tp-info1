97fa4c7bbb99f9d289776830b7eab9dac2e7069c
Nadia León <leonnadia293@gmail.com>
2025-06-08 20:20:00 -0300

    Modifico bmi.c para tp4

--- bmi.c
+++ bmi.c
@@ -10,7 +10,16 @@ int main(int argc, char *argv[]) {
 	imc=peso/(altura*altura);
 	printf("Su indice de masa corporal es %.2f\n",imc);
 	printf("Indice| Condición\n\n<18.5 | Bajo peso\n18.5 a 24.9 | Normal\n");
-	printf("25.0 a 29.9 | Sobrepeso\n>=30| Obesidad\n");
+	printf("25.0 a 29.9 | Sobrepeso\n>=30| Obesidad\n\n\n");
+	
+	
+	if (imc>= 30)
+		printf("su condicion es: obesidad\n");
+	else if (imc>= 25)
+		printf("su condicion es: sobrepeso\n");
+	else if (imc>= 18.5)
+		printf("su condicion es: normal\n");
+	else printf("su condicion es: bajo peso\n");
 	
 	return 0;
 }
